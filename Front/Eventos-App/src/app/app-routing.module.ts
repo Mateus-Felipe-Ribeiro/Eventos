ae0b4537ced79649ece508e92d33cff73fe81e6d
Mateus Ribeiro <mateus@prs.net.br>
2023-03-03 10:32:36 -0300

    Forms responsivos

--- Front/Eventos-App/src/app/app-routing.module.ts
+++ Front/Eventos-App/src/app/app-routing.module.ts
@@ -1,31 +1,77 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ContatosComponent } from './components/contatos/contatos.component';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+
+import { PerfilComponent } from './components/user/perfil/perfil.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   {
+    path: 'user',
+    redirectTo: 'user/login'
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'registration',
+        component: RegistrationComponent
+      }
+    ]
+  },
+  {
+    path: 'user/perfil',
+    component: PerfilComponent,
+  },
+  {
     path: 'eventos',
-    component: EventosComponent
+    redirectTo: 'eventos/lista'
+  },
+  {
+    path: 'eventos',
+    component: EventosComponent,
+    children:[
+      {
+        path:'lista',
+        component: EventoListaComponent,
+      },
+      {
+        path: 'detalhe',
+        component: EventoDetalheComponent,
+      },
+      {
+        path: 'detalhe/:id',
+        component: EventoDetalheComponent,
+      }
+    ]
   },
   {
     path: 'palestrantes',
-    component: PalestrantesComponent
+    component: PalestrantesComponent,
   },
   {
     path: 'dashboard',
-    component: DashboardComponent
+    component: DashboardComponent,
   },
   {
     path: 'contatos',
-    component: ContatosComponent
-  },
-  {
-    path: 'perfil',
-    component: PerfilComponent
+    component: ContatosComponent,
   },
   {
     path: '',
--- Front/Eventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/Eventos-App/src/app/components/eventos/eventos.component.ts
@@ -13,76 +13,10 @@ import { EventoService } from '../../services/evento.service';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public enviroment = 'https://localhost:5001/api/eventos';
-  public eventos: Evento[] = [];
-  public eventosFiltrados: Evento[] = [];
-  public mostrarImagem = true;
-  private filtroListado: string = '';
-  public modalRef?: BsModalRef;
 
-  public get filtroLista(): string {
-    return this.filtroListado;
-  }
+  constructor(){}
 
-  public set filtroLista(value: string) {
-    this.filtroListado = value;
-    this.eventosFiltrados = this.filtroLista
-      ? this.filtrarEventos(this.filtroLista)
-      : this.eventos;
-  }
+  ngOnInit(): void {
 
-  public filtrarEventos(filtrarPor: string): Evento[] {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      (evento: any) =>
-        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    );
-  }
-
-  constructor(
-    private eventoService: EventoService,
-    private modalService: BsModalService,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService
-  ) {}
-
-  public ngOnInit(): void {
-    this.spinner.show();
-    this.carregarElementos();
-  }
-
-  public carregarElementos(): void {
-    this.eventoService.getEventos().subscribe({
-      next: (res: Evento[]) => {
-        this.eventos = res;
-        this.eventosFiltrados = this.eventos;
-        this.spinner.hide();
-      },
-      error: (error) => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar eventos', 'Erro!');
-      },
-      complete: () => {
-        this.spinner.hide();
-      },
-    });
-  }
-
-  public mostrarImg(): void {
-    this.mostrarImagem = !this.mostrarImagem;
-  }
-
-  public openModal(template: TemplateRef<any>): void {
-    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
-  }
-
-  public confirm(): void {
-    this.modalRef?.hide();
-    this.toastr.success('maluco deu boa', 'Sucesso!');
-  }
-
-  public decline(): void {
-    this.modalRef?.hide();
   }
 }
